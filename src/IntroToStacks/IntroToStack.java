44222cbb18a380a67718c1b2d5e185d0ae3ba62a
RhymeLyet <rhymeinparentheses@gmail.com>
2017-09-10 11:29:40 -0700

    s
    s

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,17 +1,41 @@
 package IntroToStacks;
 
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> s=new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		Random r = new Random();
+		for(int i=0;i<100;i++) {
+			s.push((double)(r.nextInt(10000))/100);
+			
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		Scanner sca = new Scanner(System.in);
+		System.out.println("Enter something 0-100");
+		double x=sca.nextDouble();
+		System.out.println("Enter something 0-100");
+		double y=sca.nextDouble();
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		if(x-y>0) {
+		}
+		else {
+			double d=x;
+			x=y;
+			y=d;
+		}
+		for(int m=0;m<s.size();m++) {
+			double p=s.pop();
+			if(x>p&&y<p) {
+				System.out.println(p);
+			}
+		}
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,6 +1,14 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,5 +21,46 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
-	
+	JFrame f = new JFrame();
+	JPanel p=new JPanel();
+	JLabel l = new JLabel();
+	String text="";
+	Stack<Character> s = new Stack<Character>();
+	public TextUndoRedo(){
+		f.add(p);
+		p.add(l);
+		l.setText("");
+		f.addKeyListener(this);
+		f.setVisible(true);
+		f.setSize(400, 400);
+	}
+	public static void main(String[] args) {
+		TextUndoRedo t=new TextUndoRedo();
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode()==92) {
+			if(!s.isEmpty()) {
+				text=text+s.pop();
+			}
+		}
+		// TODO Auto-generated method stub
+		else if(e.getKeyCode()==8) {
+			s.push(text.charAt(text.length()-1));
+			text=text.substring(0,text.length()-1);
+		}
+		else {
+	//	s.push(e.getKeyChar());
+		text=text+e.getKeyChar();
+		}
+		l.setText(text);
+	}
 }
